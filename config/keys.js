83243477fda3c57febeb2945e2fdd8d792172a06
Calum Hughes <calumhughes96@gmail.com>
2018-09-01 17:19:23 +0100

    User Model & Functionality
    Added user model, user routes, validators

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://admin:adm1npass@ds241012.mlab.com:41012/socialnetworkdb"
+  mongoURI: 'mongodb://admin:adm1npass@ds241012.mlab.com:41012/socialnetworkdb',
+  secretOrKey: 'secret'
 };
